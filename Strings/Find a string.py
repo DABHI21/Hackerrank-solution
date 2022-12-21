4f924cfd158f48f9101cffb8ef1434e21e110c77
DABHI21 <jdabhi218@gmail.com>
2022-12-21 22:45:25 +0530

    Count Sub_string

---
+++ Strings/Find a string.py
@@ -0,0 +1,15 @@
+# Find how many times sub_string occurs in to string.
+
+def count_substring(string, sub_string):
+    count=0
+    for i in range(0,len(string)):
+        if sub_string in string[i:i+len(sub_string)]:
+            count+=1
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
