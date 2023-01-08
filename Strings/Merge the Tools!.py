3de3dc11216c3adfc6ad62fe68417717b4d8fae6
DABHI21 <jdabhi218@gmail.com>
2023-01-08 15:47:16 +0530

    Merge Tools

---
+++ Strings/Merge the Tools!.py
@@ -0,0 +1,13 @@
+'''
+Give string as input and split in to (string / k) parts after that you find the set of this substrings 
+and print every substring sset into newline.
+ 
+'''
+def merge_the_tools(string, k):
+    for i in range(0, len(string), k):
+        print(*dict.fromkeys(string[i : i + k]), sep="")
+    
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
